b6e3d48d0b509ea958d9dcc88692b76ef69491e1
S4bril <95976169+S4bril@users.noreply.github.com>
2026-05-04 07:43:13 +0200

    Add orchestration to the lambda handler

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
-# AWS lambda hello world function
+import json
+
+from external_api.open_meteo.open_meteo_facade import OpenMeteoFacade
+from services.weather_mapper import WeatherMapper
+
+
 def handler(event, context):
     print("Received event: " + str(event))
+    query_params = event.get("queryStringParameters") or {}
+    city = query_params.get("city") or event.get("city", "Wrocław")
+
+    open_meteo_facade = OpenMeteoFacade()
+    open_meteo_data = open_meteo_facade.get_weather(city=city)
+    weather_data = WeatherMapper.map_open_meteo_to_weather_data(open_meteo_data)
+
+    print("Mapped weather data: " + str(weather_data))
+
     return {
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': json.dumps(weather_data.__dict__)
     }
